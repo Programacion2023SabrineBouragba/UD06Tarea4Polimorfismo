39a39ac870e1654fe7b6295f0e679c2583861b65
Sabrina Bou <sabrinaanhari21@gmail.com>
2024-02-17 21:23:40 +0100

    commit inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.iesochoa.sabrinabouragba.ud06tarea4polimorfismo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.iesochoa.sabrinabouragba.ud06tarea4polimorfismo to javafx.fxml;
+    exports com.iesochoa.sabrinabouragba.ud06tarea4polimorfismo;
+}
