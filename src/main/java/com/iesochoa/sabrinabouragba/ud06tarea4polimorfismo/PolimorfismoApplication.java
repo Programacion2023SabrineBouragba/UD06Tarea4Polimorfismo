ad16eb0b91e33c5bca1e8639be92298b840a8cf0
Sabrina Bou <sabrinaanhari21@gmail.com>
2024-02-18 00:14:57 +0100

    commit controller inicio

--- src/main/java/com/iesochoa/sabrinabouragba/ud06tarea4polimorfismo/PolimorfismoApplication.java
+++ src/main/java/com/iesochoa/sabrinabouragba/ud06tarea4polimorfismo/PolimorfismoApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class PolimorfismoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PolimorfismoApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(PolimorfismoApplication.class.getResource("inicio-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 350, 300);
         stage.setTitle("UD06T4- Polimorfismo");
         stage.setScene(scene);
--- src/main/java/com/iesochoa/sabrinabouragba/ud06tarea4polimorfismo/controllers/InicioController.java
+++ src/main/java/com/iesochoa/sabrinabouragba/ud06tarea4polimorfismo/controllers/InicioController.java
@@ -1,14 +1,95 @@
 package com.iesochoa.sabrinabouragba.ud06tarea4polimorfismo.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class InicioController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btBorrarTodos;
+
+    @FXML
+    private Button btGuardar;
+
+    @FXML
+    private Button btIniciar;
+
+    @FXML
+    private Button btParar;
+
+    @FXML
+    private ColorPicker cpColor;
+
+    @FXML
+    private Pane pnPanel;
+
+    @FXML
+    private Slider slPosicionSize;
+
+    @FXML
+    private Slider slPosicionX;
+
+    @FXML
+    private Slider slPosicionY;
+
+    @FXML
+    void onClickBorrarTodo(ActionEvent event) {
+
     }
+
+    @FXML
+    void onClickCirculo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickColor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickCuadrado(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickElipse(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickGuardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickIniciar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickParar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSliderSize(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickSliderX(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClickSliderY(MouseEvent event) {
+
+    }
+
 }
